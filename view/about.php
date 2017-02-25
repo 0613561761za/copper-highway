a574b8b9a13da748b69e9d2a5125fcbd25f75f87
Austin Davis <insdavm@gmail.com>
2017-02-25 16:28:06 -0500

    Update news update and about screen

--- view/about.php
+++ view/about.php
@@ -1,12 +1,11 @@
 <div class="content">
     <h2>About</h2>
-    <p>Copper Highway was born out of the frustration of paying high prices for VPN services that we didn't control and the realization that we could easily do it better for cheaper.  </p>
-    <p>Copper Highway uses <abbr title="Named for the open-ness of its source code, not its security!">OpenVPN</abbr>, and open-source VPN implementation that provides a much higher level of security than other protocols like PPTP or IPSec. The service is built on leased Amazon Web Services (AWS) virtual private servers, which affords us full control of our infrastructure at a relatively low cost.</p>
-    <h3>Key Features</h3>
-    <h4>Cross-platform Support</h4>
+    <p>Copper Highway is a donation-funded, invite-only VPN service built from the <abbr title="Named for the open-ness of its source code, not its security!">OpenVPN</abbr> standard, an open-source VPN implementation that provides a much higher level of security than other protocols like PPTP or IPSec.</p>
+    <h4>Key Features</h4>
+    <h5>Cross-platform Support</h5>
     <p>OpenVPN is supported on Windows, OSX, *nix, iOS, and Android</p>
-    <h4>Modern Cryptography</h4>
+    <h5>Modern Cryptography</h5>
     <p>We are our own Certificate Authority (CA), a VPN industry-standard, we use only TLS 1.0+ cipher suites that support forward secrecy and never, <span class="italic">ever</span> employ outdated or broken cryptographic protocols.</p>
-    <h4>No logs</h4>
+    <h5>No logs</h5>
     <p>We don't log your VPN activity or usage, ever.</p>
 </div>
--- view/home.php
+++ view/home.php
@@ -1,5 +1,6 @@
 <div class="content">
     <p id="intro"><span class="bold">Copper Highway</span> is a community-run VPN service that provides a simple, secure solution for safeguarding your data.  Copper Highway operates solely on donations from its members.</p>
     <h3>News</h3>
+    <p><span class="italic">Feb 24, 2017</span><br/><a href="https://www.wired.com/2017/02/beware-mobile-vpns-arent-safe-seem/" target="_blank">Beware: Most Mobile VPNs Aren't as Safe as They Seem</a><br />Don't trust your data to some random commercial VPN service that will leak your personal information or IP address by using weak setups, or sell your personal information to 3rd parties!</p>
     <p><span class="italic">Jan 31, 2017</span><br/>Copper Highway (beta) is up and running!  We're starting this service out with one VPN server (located physically in Portland, OR) with plans to include more cities (and countries) if the interest is strong enough.  To join, <a href="index.php?create-account">create an account</a> and request access.  Copper Highway is an invite-only VPN service, meaning you'll need a referral code to get your account approved.</p>
 </div>
