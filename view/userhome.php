92a76d2714df5d60cc34f97f8e78921f3942d3e6
Austin Davis <insdavm@gmail.com>
2017-03-16 19:48:06 -0400

    Add custom donate button image

--- view/userhome.php
+++ view/userhome.php
@@ -1,3 +1,4 @@
+
 <?php if ( @!$d ) { $d = new UserHome(); } ?>
 <div class="content">
     <h3 class="gray">Welcome back, <?= $d->username ?>!</h3>
@@ -33,7 +34,6 @@
 			<li>Import <span class="bold"><?= $d->username; ?>.ovpn</span> into your OpenVPN client</li>
 			<li>Enter your certificate password and click connect</li>
 		    </ol>
-		    <p></p>
 
 	    <?php } ?>
 	    
@@ -49,14 +49,15 @@
 	<?php } ?>
 
 	<p></p>
+	
 	<div>
 	    Need <a href="<?= $_SERVER["PHP_SELF"]; ?>?getting-started">help</a>?
 
-	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style="display:inline;">
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" id="paypal">
 	    <input type="hidden" name="cmd" value="_s-xclick">
 	    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCFASUnlSlqS/oR7YDQdEjYpGnsFwAE6sIyS2pXtM6KX3m1ENenJbqlzkLNgluiqwaMov3AXKqRlSd6esVwyWGyLO43D/Fnp81Inbta0gvajMXKjufGq2WyrPS19Q8hQo+lBFNJXa1aeNwkj/x4lcCw/E7rLow8gbg0ZXB3qvdwmDELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIEO6VbIF9tOqAgZhEnQ8duC77ogdlHU1DviYRTXAXoFQCl2+eeVvDW3Or75ne49vhBk/64l7MoPeCyIzBBNqlV8/q76StKH2rt9hWTsZ/j9YXwGTKTmjMpNR5xLdeCr2ZIj5pm07xI15NwoMaCH132xD1+8l4XWzbjhA1MKcq9yuxoi74/JypoxvtIW+XLnzwkTaDiN9m2U2IrjxPfFjuqMLcZaCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE3MDIwMTE4NDcwNlowIwYJKoZIhvcNAQkEMRYEFNvHx6VNWS6t6RVQQ1kvIFgRJoi6MA0GCSqGSIb3DQEBAQUABIGABbbgWczU67EfmLKxmeULecgYH3AEXrRZAdlVFpqFEheP4wgK8w8IcqeataRNkqDfeP07nTIeAneGiYKmueq9sVOqW3oErxsl24ff3PiFsi1ix1+4OoszelB30lWV2B0Klz4x09smJpdLKgKvBuNEM2PzbKJnMeZVHd+N0o7R1pg=-----END PKCS7-----
 							 ">
-	    <input type="image" style="background-color: rgba(0,0,0,0);border:none;display:inline;vertical-align:middle;" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="Donate">
+	    <input type="image" src="images/donate.png" border="0" name="submit" alt="Donate">
 	    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 	</form>
 
